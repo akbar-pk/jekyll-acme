7aa718dd4a5f6cff6bc65ef980defa64b12f12e7
akbar-pk <contact.akbr@gmail.com>
2018-09-04 12:34:26 +0530

    Layers
    Added Layers

--- js/grapes/gjs.js
+++ js/grapes/gjs.js
@@ -476,6 +476,28 @@ const editor = grapesjs.init({
             }
         ]
       },
+      layerManager: {
+        appendTo: '#layers-container'
+      },
+      // We define a default panel as a sidebar to contain layers
+      panels: {
+        defaults: [{
+          id: 'layers',
+          el: '.panel__right',
+          // Make the panel resizable
+          resizable: {
+            maxDim: 350,
+            minDim: 200,
+            tc: 0, // Top handler
+            cl: 1, // Left handler
+            cr: 0, // Right handler
+            bc: 0, // Bottom handler
+            // Being a flex child we need to change `flex-basis` property
+            // instead of the `width` (default)
+            keyWidth: 'flex-basis',
+          },
+        }]
+      }
   });
 
   var blockManager = editor.BlockManager;
@@ -527,3 +549,6 @@ const editor = grapesjs.init({
       }
     ],
   });
+
+
+  
